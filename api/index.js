dd25c841add888a09d9163291b62c3c3ab7c7bf1
Rashi Garg <105194199+rashi081@users.noreply.github.com>
2024-04-23 11:20:42 +0530

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js'
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO)
+.then(()=> {
+    console.log('Connected to MongoDB')
+})
+.catch((err)=>{
+    console.log(err);
+});
+const app= express();
+
+app.use(express.json());
+
+app.listen(1322, () =>{
+    console.log("Server is running on port 1322. ");
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth',authRouter);
+
+app.use((err,req,res,next)=>{
+const statusCode= err.statusCode || 500;
+const message = err.message || 'Internal Server Error';
+return res.stustusCode(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+});
+});
